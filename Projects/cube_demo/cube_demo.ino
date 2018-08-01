13fbaa69b1dbc3b43b26c251ce65dfcbfc5076eb
Alex Lamson <alex.lamson@gmail.com>
2018-08-01 10:05:27 -0400

    Added 4 new demos

--- Projects/cube_demo/cube_demo.ino
+++ Projects/cube_demo/cube_demo.ino
@@ -26,14 +26,19 @@ void setColor(CRGB);
 void drawSmoothedPixel(float, float, float, CRGB);
 byte mapBrightness(float);
 CRGB scaleColor(CRGB, byte);
+byte getX(int);
+byte getY(int);
+byte getZ(int);
 
 CRGB leds[512];
 
 // end cube stuff
 
-byte demoMode = 2; // proj, rain, color sphere, wander, function
-const unsigned int demoTimes[] = {5000, 10000, 30000, 10000, 10000};
-const byte numDemos = 5;
+byte demoMode = 8; // proj, rain, color sphere, wander, function, trimmed sphere, bouncing in rgb space, color snake, color pyramid
+//const unsigned int demoTimes[] = {5000, 10000, 30000, 10000, 10000};
+const unsigned int demoTimes[] = {5000, 5000, 30000, 5000, 10000, 10000, 30000, 15000, 10000};
+//const unsigned int demoTimes[] = {100, 100, 100, 100, 100, 100, 100, 100, 120000};
+const byte numDemos = 9;
 
 // projection
 byte rgb[3*512];
@@ -71,9 +76,23 @@ void beginRain() {
   }
 }
 
+//math
+#define PI 3.14159265359
+
 // colors
 byte hue = 0;
 const byte value = 128; // brightness of hue
+
+
+//data snake
+int snake_frame = 0;
+
+
+//bouncing in rgb space
+int cubeScale = 8;
+float bounceX = 0, bounceY = 0, bounceZ = 0;
+float bounceDX = 0.1, bounceDY = 0.2, bounceDZ = 0.3;
+
 
 // wander
 class Wanderer {
@@ -155,6 +174,8 @@ void loop() {
         case 1: beginRain(); break;
         case 3: beginWander(); break;
         case 4: w = 0; break;
+        case 6: FastLED.clear(); break;
+        case 7: snake_frame = 0; break;
       }
     }
     
@@ -220,7 +241,85 @@ void loop() {
 //          Serial.println(millis() - startTime);
           break;
         }
-      }
+
+        case 5: // sphere with the leds off outside the sphere
+        hue += 2;
+//        setColor( CHSV( hue, 255, value ) );
+        for (byte i = 0; i < 8; i++) {
+          for (byte j = 0; j < 8; j++) {
+            for (byte k = 0; k < 8; k++) {
+              float dx = float(i) - 3.5;
+              float dy = float(j) - 3.5;
+              float dz = float(k) - 3.5;
+              float dist = sqrt( pow(dx, 2) + pow(dy, 2) + pow(dz, 2) );
+              float offset = dist * 0.25;
+              byte temp_value = value;
+              if(dist > 4) {
+                temp_value = 0;
+              }
+              leds[ getIndex( i, j, k ) ] = CHSV( hue - byte(offset * 128), 255, temp_value);
+            }
+          }
+        }
+        break;
+
+        case 6: // another dumb color thing
+        FastLED.clear();
+
+        bounceX += bounceDX;
+        if(bounceX <= 0 || bounceX >= value-8*cubeScale) {
+          bounceDX *= -1;
+        }
+        bounceY += bounceDY;
+        if(bounceY <= 0 || bounceY >= value-8*cubeScale) {
+          bounceDY *= -1;
+        }
+        bounceZ += bounceDZ;
+        if(bounceZ <= 0 || bounceZ >= value-8*cubeScale) {
+          bounceDZ *= -1;
+        }
+
+        for (byte i = 0; i < 8; i++) {
+          for (byte j = 0; j < 8; j++) {
+            for (byte k = 0; k < 8; k++) {
+              leds[ getIndex( i, j, k ) ] = CRGB( i*cubeScale+(int)bounceX, j*cubeScale+(int)bounceY, k*cubeScale+(int)bounceZ);
+            }
+          }
+        }
+        
+        break;
+
+        case 7: // color snake
+        if(snake_frame == 0) {
+          FastLED.clear();
+        }
+        for (int i = 0; i < snake_frame; i++) {
+          leds[i] = CHSV( int(1.0*i/snake_frame*255), 255, value);
+        }
+        snake_frame = (snake_frame+1) % 512;
+        break;
+
+        case 8: // color pyramid
+        hue += 4;
+        for (byte i = 0; i < 8; i++) {
+          for (byte j = 0; j < 8; j++) {
+            for (byte k = 0; k < 8; k++) {
+              float dx = float(i) - 3.5;
+              float dy = float(j) - 3.5;
+              float dist = sqrt(dx*dx + dy*dy);
+              if(dist >= 4-(k/2)) {
+                leds[ getIndex( i, j, k ) ] = CRGB( 0, 0, 0);
+              } else {
+                double angle = atan2(dy,dx)/PI;
+//                int temp_value = int(1.0*(7-k)/8*value);
+                leds[ getIndex( i, j, k ) ] = CHSV( hue - byte(angle * 128), 255, value);
+              }
+            }
+          }
+        }
+        break;
+
+      } //end of switch(demo)
 
       FastLED.show();
     }
@@ -268,6 +367,17 @@ int getIndex(int x, int y, int z) {
     y * 64;
 }
 
+byte getX(int i) {
+  return byte(i /64);
+}
+
+byte getY(int i) {
+  return byte((i/8) % 8);
+}
+
+byte getZ(int i) {
+  return byte(i % 8);
+}
 
 void setColor(CRGB color) {
   fill_solid( leds, 512, color );
