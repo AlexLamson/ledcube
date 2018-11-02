68fce4d7e22544dc336c10ba9fba14e3b2e092f0
chrisraff <crharfifs@gmail.com>
2018-11-02 16:38:41 -0400

    demo: an extra hint of standardization

--- Projects/cube_demo/SpheresIntersectingBigger.cpp
+++ Projects/cube_demo/SpheresIntersectingBigger.cpp
@@ -29,27 +29,27 @@ void SpheresIntersectingBigger::tick() {
                sphereRadius2 = radiusCenter + radiusAmplitude * cosf(angle * 0.4 + PI2 * 0.66666666);
     const float interRadius = 4.0, cosSA = cosf(angle), sinSA = sinf(angle);
     drawSphereSolid(
-      3.5 + interRadius*cosSA,
-      3.5 + interRadius*sinSA,
-      3.5,
-      sphereRadius0,
-      CRGB(0, 0, 255));
+        3.5 + interRadius*cosSA,
+        3.5 + interRadius*sinSA,
+        3.5,
+        sphereRadius0,
+        CRGB(0, 0, 255));
 
     const float cosSA1 = cosf(angle + PI2 * 0.33333333), sinSA1 = sinf(angle + PI2 * 0.33333333);
     drawSphereSolid(
-      3.5 + interRadius*cosSA1,
-      3.5 + interRadius*sinSA1,
-      3.5,
-      sphereRadius1,
-      CRGB(0, 255, 0));
+        3.5 + interRadius*cosSA1,
+        3.5 + interRadius*sinSA1,
+        3.5,
+        sphereRadius1,
+        CRGB(0, 255, 0));
 
     const float cosSA2 = cosf(angle + PI2 * 0.66666666), sinSA2 = sinf(angle + PI2 * 0.66666666);
     drawSphereSolid(
-      3.5 + interRadius*cosSA2,
-      3.5 + interRadius*sinSA2,
-      3.5,
-      sphereRadius2,
-      CRGB(255, 0, 0));
+        3.5 + interRadius*cosSA2,
+        3.5 + interRadius*sinSA2,
+        3.5,
+        sphereRadius2,
+        CRGB(255, 0, 0));
   }
 
 }
