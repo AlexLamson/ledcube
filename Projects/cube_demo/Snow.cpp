9445700226a65e497a5ead1d8a9602c53e5631a0
AlexLamson <alex.lamson@gmail.com>
2019-12-12 11:22:31 -0500

    fixed bugs, mostly works now

--- Projects/cube_demo/Snow.cpp
+++ Projects/cube_demo/Snow.cpp
@@ -17,31 +17,37 @@ Snow::Snow() {
 void Snow::initialize() {
   for (byte i = 0; i < numDrops; i++) {
     drops[i].init();
-    drops[i].z = 8 + random(8);
   }
 
-  memset(heights, 0.0, 64);
+  memset(heights, 0.0, 64 * sizeof(float));
 }
 
 void Snow::tick() {
   FastLED.clear();
 
   for (int i = 0; i < numDrops; i++) {
-
-    if (drops[i].z < heights[drops[i].x + drops[i].y*8] - 1.0) {
+    // if a snowflake lands on a snow pile, grow the pile and reset the flake
+    if (drops[i].z < heights[drops[i].x + 8*drops[i].y]) {
+      heights[drops[i].x + 8*drops[i].y] += 1.0f;//0.5;
       drops[i].init();
-      heights[drops[i].x + drops[i].y*8] += 0.5;
     }
 
+    // draw each snowflake
     drops[i].tick();
-
     drops[i].draw();
   }
 
+  // draw the snow piles
   for (int i = 0; i < 64; i++) {
     byte x = i/8, y = i%8;
-    heights[i] = max (0, heights[i] - 0.001);
-    drawLine(x + 0.01, y + 0.01, 0, x, y, heights[i], 0xFFFFFF);
+
+    // taller piles melt faster
+    if(randomf() < 0.01f*(heights[i]+1)/8.0f) {
+      heights[i] -= 0.001f;
+    }
+    heights[i] = max(heights[i], 0);
+    heights[i] = min(heights[i], 8);
+    drawLine(x + 0.01, y + 0.01, 0, x, y, heights[i]-1, 0xFFFFFF);
   }
 }
 
@@ -52,23 +58,17 @@ Snow::Drop::Drop() {
 void Snow::Drop::init() {
   x = random(8);
   y = random(8);
-  z = 8;
-
-  dz = 0.1 + float(random(10)) / 100;
+  z = 8 + random(8);
+  dz = 0.1 + 0.1f*randomf();
 }
 void Snow::Drop::tick() {
   z -= dz;
 
-  // reset if too low
-  if (z < -4) {
+  // reset if too low (unlikely, should hit snow pile first)
+  if (z < -2) {
     init();
   }
 }
 void Snow::Drop::draw() {
-  float currZ = z;
-  const byte length = 1;
-  while (currZ - z < length) {
-    drawSmoothedPixel(x, y, currZ, 0xFFFFFF);
-    currZ += 1;
-  }
+  drawSmoothedPixel(x, y, z, 0xFFFFFF);
 }
